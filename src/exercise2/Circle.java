1680185bb5eb39f30c8e5d0b270b6a9f5f7f22c0
VSherova <>
2021-10-26 17:31:38 +0000

    Files updated

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -7,8 +7,7 @@ public class Circle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter  radius: ");
-        double r = input.nextDouble();
+        System.out.print("Enter  radius: ");        double r = input.nextDouble();
         double c = 2 * Math.PI * r; 
         double a = Math.PI * r * r;
         System.out.println("Circumference: " + c);
